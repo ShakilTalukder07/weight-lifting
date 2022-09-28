92a9b6e33e402710595cc3f794ad84ea34277b3b
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-09-28 23:01:52 +0600

    card adedd

---
+++ src/components/Calculations/Calculations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Calculations.css'
+
+const Calculations = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Calculations;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className="left-side">
+                <h1>i am left</h1>
+            </div>
+            <div className="right-side">
+                <h1>i am right</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
