35a209da238dd5f8f52b09d4bd8e9790617415a2
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-09-29 21:11:09 +0600

    Project Complete

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css'
-import logo from '../../images/logo.png'
+import logo from '../../images/push.jpg'
 
 const Header = () => {
     return (
         <nav className='nav'>
             <div className="nav-logo">
                 <img className="logo" src={logo} alt="" />
+                {/* <img src="" alt="" /> */}
                 <h1>Weight Lifting</h1>
             </div>
         </nav>
